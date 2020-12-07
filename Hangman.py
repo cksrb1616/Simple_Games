c80bbaa495af21721221b43058348e8eeab8a1cf
cksrb1616 <53897355+cksrb1616@users.noreply.github.com>
2020-12-07 15:01:37 +0100

    changes

--- Hangman.py
+++ Hangman.py
@@ -1,15 +1,21 @@
 # pip install random-word
+#from random_word import RandomWords
 
-from random_word import RandomWords
 from random import *
+import getpass
 
-words = RandomWords()
-word = words.get_random_word()
+#word = RandomWords()
+#word.get_random_word()
+#words = ['orange', 'apple', 'banana']
+word = getpass.getpass("Input your word: ").lower()
+
+print("answer: "+ word)
+letters = ""
 # r = RandomWords()
 # r.get_random_words() : in list
 # r.word_of_the_day() : Return Word of the day
-print("answer: "+ word)
-letters = ""
+
+count = 0
 
 while True:
     print()
@@ -25,10 +31,15 @@ while True:
         print("success")
         break
 
-    letter = input("Input letter > ")
+    letter = input("Input letter > ").lower()
+
     if letter not in letters:
         letters += letter
     if letter in word:
         print("correct")
     else:
         print('wrong')
+        count += 1
+        if count == 5:
+            print('game over')
+            break
